c4a3a4e6b779a64d4888586b34812f5cb8b2b9a0
csengebalogh <csenge.balogh1214@gmail.com>
2022-03-23 15:58:55 +0100

    initial commit

---
+++ src/App.java
@@ -0,0 +1,17 @@
+public class App {
+  public static void main(String[] args) throws Exception {
+    System.out.println("2022.03.23, Balogh Csenge, Szoft_I_N");
+    System.out.println("Személyek adatainak bekérése");
+  }
+
+  public static void getData () {
+    try {
+
+    } catch () {
+
+    }
+
+  public static void tryGetData() {
+        
+  }
+}
